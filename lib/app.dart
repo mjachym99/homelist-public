cb0a61a945c661831279e2eb650e7a69cd36c8a1
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2023-05-14 22:45:10 +0200

    configure firebase, added auth and firestore plugins

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homelist/application/auth/auth_cubit.dart';
-import 'package:homelist/application/auth/auth_state.dart';
 import 'package:homelist/application/core/navigation.dart';
 
 class HomeList extends StatelessWidget {
@@ -15,23 +14,20 @@ class HomeList extends StatelessWidget {
           create: (context) => AuthCubit(),
         ),
       ],
-      child: BlocBuilder<AuthCubit, AuthState>(builder: (context, state) {
-        return MaterialApp.router(
-          title: 'HomeList',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 182, 226, 81),
-            ),
-            useMaterial3: true,
+      child: MaterialApp.router(
+        title: 'HomeList',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 182, 226, 81),
           ),
-          debugShowCheckedModeBanner: false,
-          routeInformationParser:
-              NavigationService.router.routeInformationParser,
-          routeInformationProvider:
-              NavigationService.router.routeInformationProvider,
-          routerDelegate: NavigationService.router.routerDelegate,
-        );
-      }),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: NavigationService.router.routeInformationParser,
+        routeInformationProvider:
+            NavigationService.router.routeInformationProvider,
+        routerDelegate: NavigationService.router.routerDelegate,
+      ),
     );
   }
 }
